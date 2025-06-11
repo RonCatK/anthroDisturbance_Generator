6d5bdff4a1646d94357a9ecd6763107229844f33
RonCatK <ronald.kilian@protonmail.com>
2025-06-11 17:24:07 +0200

    refactored saveDisturbances to:
    Unify vector & raster logic in a single write_one() helper
    
    Consolidate class checks using any(...) + is(..., "Spatial")
    
    Simplify layer‐name handling (optional layer argument)
    
    Streamline warning & error messages with sprintf()
    
    Remove duplicate code paths

--- R/saveDisturbances.R
+++ R/saveDisturbances.R
@@ -2,86 +2,59 @@ saveDisturbances <- function(disturbanceList,
                              currentTime,
                              overwrite,
                              runName) {
-  lapply(names(disturbanceList), function(Sector) {
-    whichLay <- names(disturbanceList[[Sector]])[!grepl(x = names(disturbanceList[[Sector]]),
-                                                        pattern = "potential")]
-    obj <- disturbanceList[[Sector]]
-    # new test: any terra/spatVector or sf or raster, or any sp
+  # Helper to write one spatial object
+  write_one <- function(obj, sector, layer = NULL) {
+    # decide extension and write func
+    if (is.null(obj)) {
+      warning(sprintf("The layer for %s%s is NULL. Not saving.", sector,
+                      if (!is.null(layer)) paste0(" -- ", layer) else ""),
+              immediate. = TRUE)
+      return()
+    }
+    # Coerce and write vector
+    if (any(class(obj) %in% c("SpatVector", "sf")) || is(obj, "Spatial")) {
+      if (!inherits(obj, "SpatVector")) obj <- terra::vect(obj)
+      fname <- paste0("disturbances_", sector,
+                      if (!is.null(layer)) paste0("_", layer) else "",
+                      "_", currentTime, "_", runName, ".shp")
+      terra::writeVector(obj, file.path(Paths$outputPath, fname),
+                         filetype = "ESRI Shapefile", overwrite = overwrite)
+    }
+    # Raster branch
+    else if (any(class(obj) %in% c("RasterLayer", "SpatRaster"))) {
+      if (inherits(obj, "RasterLayer")) obj <- terra::rast(obj)
+      fname <- paste0("disturbances_", sector,
+                      if (!is.null(layer)) paste0("_", layer) else "",
+                      "_", currentTime, "_", runName, ".tif")
+      terra::writeRaster(obj, file.path(Paths$outputPath, fname),
+                         filetype = "GTiff", overwrite = overwrite)
+    } else {
+      stop(sprintf("Objects of class %s can't be used. Please use raster, sp, sf, or terra formats.",
+                   paste(class(obj), collapse=",")))
+    }
+  }
+  
+  for (sector in names(disturbanceList)) {
+    obj <- disturbanceList[[sector]]
+    # flat list: single object
     if (any(class(obj) %in% c("SpatVector", "sf", "RasterLayer", "SpatRaster")) ||
         is(obj, "Spatial")) {
-      
-      # The class here is expected to be a list. But we might have cases when it is not
-      message(paste0("Layer: ", Sector, " was likely not generated (i.e., lack of potential ",
-                     "disturbance layer). Saving current disturbance layer (i.e., this layer may ",
-                     "not vary through time!)"))
-      Lay <- disturbanceList[[Sector]]
-      if (any(class(Lay) %in% c("SpatVector", "sf"),
-              is(Lay, "Spatial"))) {
-        if (!is(Lay, "SpatVector"))
-          Lay <- vect(Lay)
-        terra::writeVector(x = Lay,
-                           filename = file.path(Paths[['outputPath']],
-                                                paste0("disturbances_", Sector, "_",
-                                                       currentTime, "_",
-                                                       runName, ".shp")),
-                           filetype = "ESRI Shapefile", overwrite = overwrite)
-      } else {
-        if ( any(class(Lay) %in% c("RasterLayer", "SpatRaster"))) {
-          if (is(Lay, "RasterLayer"))
-            Lay <- rast(Lay)
-          terra::writeRaster(x = Lay, filename = file.path(Paths[['outputPath']],
-                                                           paste0("disturbances_", Sector, "_",
-                                                                  currentTime, "_",
-                                                                  runName, ".tif")),
-                             filetype = "GTiff", overwrite = overwrite)
-        } else {
-          if (is.null(Lay)) {
-            warning(paste0("The layer for ", Sector, " -- ", LAYER, " is NULL. Not saving."),
-                    immediate. = TRUE)
-          } else stop(paste0("Objects of class ", class(Lay), "can't be used in the module. ",
-                             "Please make sure all spatial objects are of raster, sp, sf or terra formats."))
+      message(sprintf("Layer: %s was likely not generated. Saving current disturbance.", sector))
+      write_one(obj, sector)
+    } else if (is.list(obj)) {
+      # multi-layer
+      layers <- setdiff(names(obj), grep("potential", names(obj), value=TRUE))
+      for (lay in layers) {
+        message(sprintf("Saving layer: %s -- %s", sector, lay))
+        layer_obj <- obj[[lay]]
+        # preserve or assign Class
+        if (any(class(layer_obj) %in% c("SpatVector", "sf")) || is(layer_obj, "Spatial")) {
+          if (!"Class" %in% names(layer_obj) || all(is.na(layer_obj$Class)))
+            layer_obj$Class <- lay
         }
+        write_one(layer_obj, sector, lay)
       }
-    } else {
-      lapply(whichLay, function(LAYER) {
-        Lay <- disturbanceList[[Sector]][[LAYER]]
-        message(paste0("Saving layer: ", Sector, " -- ", LAYER))
-        # only add a Class column if Lay is non-null vector and either has no Class or it’s all NA
-        if (!is.null(Lay) && any(class(Lay) %in% c("SpatVector", "sf"), is(Lay, "Spatial"))) {
-          fld <- names(Lay)
-          if (!"Class" %in% fld || all(is.na(Lay$Class))) {
-            Lay$Class <- LAYER
-          }
-        }
-        if (any(class(Lay) %in% c("SpatVector", "sf"),
-                is(Lay, "Spatial"))) {
-          if (!is(Lay, "SpatVector"))
-            Lay <- vect(Lay)
-          terra::writeVector(x = Lay,
-                             filename = file.path(Paths[['outputPath']],
-                                                  paste0("disturbances_", Sector, "_", LAYER, "_",
-                                                         currentTime, "_",
-                                                         runName, ".shp")),
-                             filetype = "ESRI Shapefile", overwrite = overwrite)
-        } else {
-          if ( any(class(Lay) %in% c("RasterLayer", "SpatRaster"))) {
-            if (is(Lay, "RasterLayer"))
-              Lay <- rast(Lay)
-            terra::writeRaster(x = Lay, filename = file.path(Paths[['outputPath']],
-                                                             paste0("disturbances_", Sector, "_", LAYER, "_",
-                                                                    currentTime, "_",
-                                                                    runName, ".tif")),
-                               filetype = "GTiff", overwrite = overwrite)
-          } else {
-            if (is.null(Lay)) {
-              warning(paste0("The layer for ", Sector, " -- ", LAYER, " is NULL. Not saving."),
-                      immediate. = TRUE)
-            } else stop(paste0("Objects of class ", class(Lay), "can't be used in the module. ",
-                               "Please make sure all spatial objects are of raster, sp, sf or terra format."))
-          }
-        }
-      })
     }
-  })
-  message(crayon::green(paste0("All disturbances saved for ", currentTime)))
+  }
+  message(crayon::green(sprintf("All disturbances saved for %s", currentTime)))
 }
